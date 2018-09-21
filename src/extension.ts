e39a939e4ea9bb8e2e312b916ccf0669b57411a0
道化師 <wraith13@hotmail.co.jp>
2018-09-21 16:04:01 +0900

    🛠 実装

--- src/extension.ts
+++ src/extension.ts
@@ -2,37 +2,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as os from 'os';
 
 export module UnsavedFiles
 {
-    var pass_through;
-
-    const practicalTypeof = function(obj : any) : string
-    {
-        if (undefined === obj)
-        {
-            return "undefined";
-        }
-        if (null === obj)
-        {
-            return "null";
-        }
-        if ("[object Array]" === Object.prototype.toString.call(obj))
-        {
-            return "array";
-        }
-
-        return typeof obj;
-    };
-
-    function getConfiguration<type>(key? : string, section : string = "unsaved-files") : type
-    {
-        const configuration = vscode.workspace.getConfiguration(section);
-        return key ?
-            configuration[key] :
-            configuration;
-    }
+    let pass_through;
 
     export function registerCommand(context : vscode.ExtensionContext): void
     {
@@ -40,390 +13,41 @@ export module UnsavedFiles
         (
             vscode.commands.registerCommand
             (
-                'unsaved-files-vscode.showSystemInformation', showSystemInformation
+                'unsaved-files.show', show
             )
         );
     }
 
-    interface GetSystemInformationOptions
+    export async function show() : Promise<void>
     {
-        categories : string[];
-        withSensitiveData : boolean;
-        withInternalExtensions : boolean;
-    }
-
-    export function getSystemInformation(options : GetSystemInformationOptions) : object
-    {
-        const thisExtension = vscode.extensions.getExtension("wraith13.unsaved-files-vscode");
-        return pass_through =
+        const unsavedDocuments = vscode.workspace.textDocuments.filter(i => i.isDirty || i.isUntitled);
+        if (unsavedDocuments.length <= 0)
         {
-            "timestamp": new Date().toISOString(),
-            "provider":
-            {
-                "id": thisExtension && thisExtension.id,
-                "name": thisExtension && thisExtension.packageJSON.name,
-                "displayName": thisExtension && thisExtension.packageJSON.displayName,
-                "version": thisExtension && thisExtension.packageJSON.version,
-                "options": options,
-            },
-            "warning": undefined, // 表示位置をここにする為に undefined で事前挿入
-            "os": 0 <= options.categories.indexOf("basic") ?
-            {
-                "arch": os.arch(),
-                "platform": os.platform(),
-                "type": os.type(),
-                "release": os.release(),
-                "EOL": JSON.stringify(os.EOL),
-                "endianness": os.endianness(),
-                "cpus": 0 <= options.categories.indexOf("cpu") ? os.cpus(): undefined,
-                "totalmem": 0 <= options.categories.indexOf("memory") ? os.totalmem(): undefined,
-                "freemem": 0 <= options.categories.indexOf("memory") ? os.freemem(): undefined,
-                "networkInterfaces": 0 <= options.categories.indexOf("network") && options.withSensitiveData ? os.networkInterfaces(): undefined,
-                "hostname": options.withSensitiveData ? os.hostname(): undefined,
-                "homedir": options.withSensitiveData ? os.homedir(): undefined,
-                "tmpdir": options.withSensitiveData ? os.tmpdir(): undefined,
-            } : undefined,
-            "process": 0 <= options.categories.indexOf("basic") ?
-            {
-                "versions": process.versions,
-                "arch": process.arch,
-                "execPath": options.withSensitiveData ? process.execPath: undefined,
-                "execArgv": options.withSensitiveData ? process.execArgv: undefined,
-                "env":
-                {
-                    "LANG": process.env.LANG,
-                }
-            } : undefined,
-            "vscode":
-            {
-                "version": vscode.version,
-                "env":
-                {
-                    "appName": vscode.env.appName,
-                    "language": vscode.env.language
-                },
-                "extensions": 0 <= options.categories.indexOf("extensions") ? getExtentionsformation(options): undefined,
-            },
-        };
-    }
-    interface Extension
-    {
-        id : string;
-    }
-    function isInternalExtension(extension : Extension) : boolean
-    {
-        return extension.id.startsWith("vscode.");
-    }
-    export function getExtentionsformation(options : GetSystemInformationOptions) : object
-    {
-        return vscode.extensions.all
-        .filter(extension => (options.withInternalExtensions || !isInternalExtension(extension)))
-        .map
-        (
-            extension => pass_through =
-            {
-                "id": extension.id,
-                "isActive": extension.isActive,
-                "extensionPath": options.withSensitiveData ? extension.extensionPath: undefined,
-                "packageJSON":
-                {
-                    "name": extension.packageJSON.name,
-                    "version": extension.packageJSON.version,
-                    "displayName": extension.packageJSON.displayName,
-                    "description": extension.packageJSON.description,
-                    "publisher": extension.packageJSON.publisher,
-                    "categories": extension.packageJSON.categories,
-                }
-            }
-        );
-    }
-    export function systemLint(information : any) : any
-    {
-        if ("darwin" === information["os"]["platform"] && undefined === information["process"]["env"]["LANG"])
-        {
-            information["warnings"] = information["warnings"] || { };
-            information["warnings"]["W001"] = "LANG environment variable is empty. Clipboard related extensions do not work correctly with Non-ASCII characters. To avoid this problem, you should launch vscode from Terminal.app. See <https://github.com/Microsoft/vscode/issues/16261>.";
+            await vscode.window.showInformationMessage("No unsaved files");
         }
-        return information;
-    }
-    export function hideInformation(information : any) : any
-    {
-        getConfiguration<string[]>("hideItems").forEach
-        (
-            path =>
-            {
-                var parents = [information];
-                path.split(".").forEach
+        else
+        {
+            const selectedFile = await vscode.window.showQuickPick
+            (
+                unsavedDocuments.map
                 (
-                    (key, index, keys) =>
+                    i => pass_through =
                     {
-                        if (0 < parents.length)
-                        {
-                            if (index +1 < keys.length)
-                            {
-                                if ("*" === key)
-                                {
-                                    parents = parents
-                                        .map
-                                        (
-                                            parent => Object.keys(parent)
-                                                .map(i => parent[i])
-                                                .filter(i => undefined !== i)
-                                        )
-                                        .reduce((a,b) => a.concat(b));
-                                }
-                                else
-                                {
-                                    parents = parents
-                                        .map(parent => parent[key])
-                                        .filter(parent => undefined !== parent);
-                                }
-                            }
-                            else
-                            {
-                                parents.forEach(parent => delete parent[key]);
-                            }
-                        }
+                        "label": i.fileName,
+                        "description": "", // `lines:${i.lineCount}`,
+                        "detail": i.languageId
                     }
-                );
-            }
-        );
-        return information;
-    }
-
-    async function openNewTextDocument(language : string) : Promise<vscode.TextDocument>
-    {
-        return await vscode.workspace.openTextDocument({ language });
-    }
-    async function showQuickPick
-    (
-        items : vscode.QuickPickItem[],
-        options: vscode.QuickPickOptions,
-        autoSelectedIndex? : number
-    )
-    : Promise<vscode.QuickPickItem|undefined>
-    {
-        return undefined === autoSelectedIndex ?
-            await vscode.window.showQuickPick(items, options):
-            items[autoSelectedIndex];
-    }
-    async function openNewCodeDocument(language : string, code : string) : Promise<void>
-    {
-        const document = await openNewTextDocument(language);
-        const textEditor = await vscode.window.showTextDocument(document);
-        textEditor.edit
-        (
-            (editBuilder: vscode.TextEditorEdit) =>
-            {
-                editBuilder.insert(new vscode.Position(0,0), code);
-            }
-        );
-    }
-    export async function showSystemInformation() : Promise<void>
-    {
-        const selectedCategories =  await showQuickPick
-        (
-            [
+                ),
                 {
-                    "label": "Basic Unsaved Files",
-                    "description": "",
-                    "detail": "basic, extensions"
-                },
-                {
-                    "label": "Full Unsaved Files",
-                    "description": "Becouse this data include sensitive data, recommend to keep private.",
-                    "detail": "basic, cpu, memory, network, extensions"
-                }
-            ],
-            {
-                placeHolder: "Select categories option",
-            }
-        );
-        if (!selectedCategories)
-        {
-            return;
-        }
-        const categories = (selectedCategories.detail || "").split(",").map(i => i.trim());
-        const isFull = selectedCategories.label.toLowerCase().startsWith("full ");
-        const information = getSystemInformation
-        (
-            {
-                categories: categories,
-                withSensitiveData: isFull,
-                withInternalExtensions: isFull,
-            }
-        );
-        systemLint(information);
-        hideInformation(information);
-        const format =  await vscode.window.showQuickPick
-        (
-            [
-                {
-                    "label": "Markdown",
-                    "description": "",
-                    "detail": "markdown"
-                },
-                {
-                    "label": "JSON",
-                    "description": "",
-                    "detail": "json"
-                }
-            ],
-            {
-                placeHolder: "Select a format",
-            }
-        );
-        if (!format)
-        {
-            return;
-        }
-        await openNewCodeDocument
-        (
-            format.detail,
-            "json" === format.detail ?
-                JSON.stringify(information, null, 4):
-                informationToMarkdown(information)
-        );
-    }
-    function escapeMarkdown(text : string) : string
-    {
-        return text.replace(/\\/g, "\\\\");
-    }
-    function makeMarkdownHeader(level : number, title : string) : string
-    {
-        return `${"#".repeat(level)} ${escapeMarkdown(title)}\n`;
-    }
-    function makeMarkdownTable(data : [{key:string,value:any}]) : string
-    {
-        return pass_through =
-        [
-            "| key | value |",
-            0 < data.filter(i => "number" !== practicalTypeof(i.value)).length ? "|---|---|": "|---|---:|",
-        ]
-        .concat
-        (
-            data.map
-            (
-                i =>
-                    "string" === practicalTypeof(i.value) ? `| ${escapeMarkdown(i.key)} | ${escapeMarkdown(i.value)} |`:
-                    "number" === practicalTypeof(i.value) ? `| ${escapeMarkdown(i.key)} | ${escapeMarkdown(i.value.toLocaleString())} |`:
-                    `| ${escapeMarkdown(i.key)} | ${escapeMarkdown(JSON.stringify(i.value))} |`
-            )
-        )
-        .join("\n") +"\n";
-    }
-    function makeMarkdown(data : any, level : number = 2, title? : string, isExtensionData : boolean = false) : string | undefined
-    {
-        if (!data)
-        {
-            return undefined;
-        }
-
-        const extensionLinks = (isExtensionData && data && data.id && !isInternalExtension(data)) ?
-            [
-                `- [open in marketplace](https://marketplace.visualstudio.com/items?itemName=${data.id})\n`,
-                `- [open in vscode](vscode:extension/${data.id})\n`
-            ]
-            .join(""):
-            undefined;
-
-        const tableItems : [{key:string,value:any}] = <any>[];
-        const arrayItems : [{key:string,value:[any]}] = <any>[];
-        const subTables : [{key:string,value:any}] = <any>[];
-        Object.keys(data)
-            .filter(key => undefined !== data[key])
-            .forEach
-            (
-                key =>
-                {
-                    const value = data[key];
-                    const type = practicalTypeof(value);
-                    if
-                    (
-                        ("object" !== type || 0 === Object.keys(value).filter(i => undefined !== value[i]).length) &&
-                        ("array" !== type || 0 === value.length || "string" === practicalTypeof(value[0]))
-                    )
-                    {
-                        tableItems.push({key: key, value: value});
-                    }
-                    else
-                    if ("array" === type)
-                    {
-                        arrayItems.push({key: key, value: value});
-                    }
-                    else
-                    {
-                        subTables.push({key: key, value: value});
-                    }
+                    placeHolder: "Select a unsaved file",
                 }
             );
-        if (isExtensionData && data && data.packageJSON)
-        {
-            subTables.pop();
-            Object.keys(data.packageJSON)
-                .filter(key => undefined !== data.packageJSON[key])
-                .forEach
-                (
-                    key =>
-                    {
-                        if (undefined !== data.packageJSON[key])
-                        {
-                            tableItems.push({key: key, value: data.packageJSON[key]});
-                        }
-                    }
-                );
+            if (selectedFile)
+            {
+                const selectedDocument = unsavedDocuments.filter(i => i.fileName === selectedFile.label)[0];
+                vscode.window.showTextDocument(selectedDocument);
+            }
         }
-
-        return pass_through =
-        [
-            title ? makeMarkdownHeader(level, title): undefined,
-            extensionLinks,
-            0 < tableItems.length ? makeMarkdownTable(tableItems): undefined,
-        ]
-        .concat(subTables.map(i => makeMarkdown(i.value, level + 1, i.key, isExtensionData)))
-        .concat
-        (
-            arrayItems.map
-            (
-                i => pass_through =
-                [
-                    makeMarkdownHeader(level +1, i.key),
-                    undefined
-                ]
-                .concat
-                (
-                    i.value.map
-                    (
-                        (j : any, index : number) => makeMarkdown
-                        (
-                            j,
-                            level +2,
-                            isExtensionData && j && j.packageJSON && j.packageJSON.displayName ? j.packageJSON.displayName: `${i.key}.${index}`,
-                            isExtensionData
-                        )
-                    )
-                )
-                .filter(i => undefined !== i)
-                .join("\n")
-            )
-        )
-        .filter(i => undefined !== i)
-        .join("\n");
-    }
-    export function informationToMarkdown(information : any) : string
-    {
-        return pass_through =
-        [
-            makeMarkdownHeader(1, "VS Code Unsaved Files"),
-            information["timestamp"] && `timestamp: ${information["timestamp"]}\n`,
-            makeMarkdown(information["provider"], 2, "Information Provider", true),
-            makeMarkdown(information["warnings"], 2, "Warnings"),
-            makeMarkdown(information["os"], 2, "OS Information"),
-            makeMarkdown(information["process"], 2, "Process Information"),
-            makeMarkdown(information["vscode"], 2, "VS Code Information", true),
-        ]
-        .filter(i => undefined !== i)
-        .join("\n");
     }
 
     //  dummy for test
